038da542b87182113314fdecbc84296c8f174da7
swapnil3597 <swapnil3597@gmail.com>
2020-04-08 07:30:04 +0000

    run inference

---
+++ run_inference.sh
@@ -0,0 +1,9 @@
+python run.py \
+ --mode=infer \
+ --config="example_configs/speech2text/jasper10x5_LibriSpeech_nvgrad_masks.py" \
+ --logdir="../checkpoint/" \
+ --num_gpus=1 \
+ --use_horovod=False \
+ --decoder_params/use_language_model=False \
+ --infer_output_file=model_output.pickle
+
